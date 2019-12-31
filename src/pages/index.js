bb39a75f55c55e11d5ad88cbb5eb4cdc6eaffcd4
AKBasculas <ivan@akbasculas.com>
2019-12-31 12:23:38 -0800

    Started portfolio website

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,11 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+function Index(props) {
+    return (
+        <div>
+            Iván León
+        </div>
+    );
+}
+
+export default Index;
